ec7cb83afe9c59311703c998092087d2cb034bf5
Rohan Jhaldiyal <rohanjhaldiyal@gmail.com>
2024-01-04 22:54:01 +0530

    Fix file upload

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,6 +16,7 @@ const uploadOnCloudinary = async (localFilePath) => {
     });
     //file has been uploaded successfully
     console.log("file is uploaded oncloudinary", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
